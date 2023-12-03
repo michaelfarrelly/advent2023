f4887d9c16a086859a71276af89aecd6701535e2
Michael Farrelly <mikefarrelly@gmail.com>
2023-12-03 13:34:08 -0700

    day2.part1.2

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,6 @@
 use std::{env, fs};
 
-fn parse_round(input: &str) -> (u32, u32, u32) {
+fn parse_round_item(input: &str) -> (u32, u32, u32) {
     // 3 blue, 4 red;
     // 1 red, 2 green, 6 blue;
     // 2 green
@@ -9,8 +9,8 @@ fn parse_round(input: &str) -> (u32, u32, u32) {
     let mut g: u32 = 0;
     let mut b: u32 = 0;
 
-    let pieces = input.split(",").map(|i| {
-        let parts: Vec<&str> = i.trim().split(" ").collect();
+    let pieces = input.split(",").map(|line| {
+        let parts: Vec<&str> = line.trim().split(" ").collect();
         let num = parts[0].parse::<u32>().unwrap_or(0);
         let color = parts[1];
         (num, color)
@@ -28,6 +28,41 @@ fn parse_round(input: &str) -> (u32, u32, u32) {
     return (r, g, b);
 }
 
+fn parse_round(input: &str, max_balls: (u32, u32, u32)) -> Option<u32> {
+    let mut round_values = input.split(",").map(|i| {
+        let r_values = parse_round_item(i);
+
+        if r_values.0 <= max_balls.0 && r_values.1 <= max_balls.1 && r_values.2 <= max_balls.2 {
+            Some(1)
+        } else {
+            None
+        }
+    });
+
+    let all_valid_items = round_values.all(|v| v.is_some());
+
+    if all_valid_items {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn parse_game(game_id: u32, input: &str, max_balls: (u32, u32, u32)) -> Option<u32> {
+    let rounds = input.split(";");
+    let mut round_items = rounds.map(|r| parse_round(r, max_balls));
+    let all_valid_rounds = round_items.all(|v| v.is_some());
+
+    if all_valid_rounds {
+        println!("Game {game_id} [{all_valid_rounds}] {input} -> {all_valid_rounds}");
+    }
+    if all_valid_rounds {
+        Some(game_id)
+    } else {
+        None
+    }
+}
+
 fn part1(content: String) -> u32 {
     let max_balls = (12, 13, 14);
 
@@ -35,39 +70,17 @@ fn part1(content: String) -> u32 {
         // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
         let s1: Vec<&str> = line.split(":").collect();
         let game_id = s1[0].replace("Game ", "").parse::<u32>().unwrap_or(0);
-        let rounds = s1[1].trim().split(";");
-        let mut round_items = rounds.map(|r| {
-            let mut round_values = r.split(",").map(|i| {
-                let r_values = parse_round(i);
+        // let rounds = s1[1].trim().split(";");
+        // let mut round_items = rounds.map(|r| parse_round(r, max_balls));
+        // let all_valid_rounds = round_items.all(|v| v.is_some());
 
-                if r_values.0 <= max_balls.0
-                    && r_values.0 <= max_balls.0
-                    && r_values.0 <= max_balls.0
-                {
-                    Some(1)
-                } else {
-                    None
-                }
-            });
-
-            let all_valid_items = round_values.all(|v| v.is_some());
-
-            if all_valid_items {
-                Some(1)
-            } else {
-                None
-            }
-        });
-        let all_valid_rounds = round_items.all(|v| v.is_some());
-
-        if all_valid_rounds {
-            Some(game_id)
-        } else {
-            None
-        }
-
-        // println!("Game {game_id} {sum1}");
-        // Some(game_id)
+        // println!("Game {game_id} {all_valid_rounds}");
+        // if all_valid_rounds {
+        //     Some(game_id)
+        // } else {
+        //     None
+        // }
+        parse_game(game_id, s1[1].trim(), max_balls)
     });
 
     return result.sum();
